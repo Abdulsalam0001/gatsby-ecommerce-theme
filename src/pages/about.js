bdfd539848384f2e1bbf76b8a41747ebfbc4b76b
chris chase <dreshaw2012@gmail.com>
2022-06-30 17:36:28 +0100

    i

--- src/pages/about.js
+++ src/pages/about.js
@@ -27,7 +27,7 @@ const AboutPage = (props) => {
         <Hero
           maxWidth={'900px'}
           image={'/about.png'}
-          title={`Sydney \n A British brand since 1860`}
+          title={`Masters Of Arts \n A Nigeria brand since 1860`}
         />
 
         <div className={styles.navContainer}>
